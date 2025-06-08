7dd7080274b567002c6e2be7a307e22569387672
Devansh Singh Kushwah <devanshs843@gmail.com>
2025-06-09 01:25:22 +0530

    Build fixes

--- app/page.tsx
+++ app/page.tsx
@@ -11,62 +11,67 @@ import { AnimatePresence } from "framer-motion";
 import { useEffect, useState } from "react";
 // import Lenis from 'lenis';
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
 
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [isMounted, setIsMounted] = useState<boolean>(false);
 
-  // useEffect( () => {
+    // useEffect( () => {
 
-  //   const lenis = new Lenis()
-
-
-  //   function raf(time: number) {
-
-  //     lenis.raf(time)
-
-  //     requestAnimationFrame(raf)
-
-  //   }
+    //   const lenis = new Lenis()
 
 
+    //   function raf(time: number) {
+
+    //     lenis.raf(time)
+
+    //     requestAnimationFrame(raf)
+
+    //   }
 
 
-  //   requestAnimationFrame(raf)
 
-  // }, [])
 
-  useEffect(() => {
-    (
-      async () => {
-        const LocomotiveScroll = (await import('locomotive-scroll')).default
-        const locomotiveScroll = new LocomotiveScroll();
+    //   requestAnimationFrame(raf)
 
-        setTimeout(() => {
-          setIsLoading(false);
-          document.body.style.cursor = "default";
-          window.scrollTo(0, 0);
+    // }, [])
+
+    useEffect(() => {
+        // if (typeof window === 'undefined') return;
+        setIsMounted(true);
+        let timerId = setTimeout(() => {
+            setIsLoading(false);
+            document.body.style.cursor = "default";
+            window.scrollTo(0, 0);
         }, 2000);
-      }
-    )()
-  }, []);
 
-  return (
-    <>
-      <AnimatePresence mode="wait">
-        {isLoading && <PreLoader />}
-      </AnimatePresence>
-      {/* <main className="relative overflow-hidden flex justify-center items-center flex-col mx-auto"> */}
-      <main className="relative overflow-hidden">
-        <Hero />
-        {/* <Experience /> */}
-        <div className="max-w-7xl mx-auto">
-          <About />
-          <Projects />
-          <Clients />
-        </div>
-        <Contact />
 
-      </main>
-    </>
-  );
+        return () => {
+            clearTimeout(timerId);
+        }
+    }, []);
+
+    if (!isMounted) {
+        return null;
+    }
+
+    return (
+        <>
+            <AnimatePresence mode="wait">
+                {isLoading && <PreLoader />}
+            </AnimatePresence>
+            <main className="relative overflow-hidden">
+                <Hero />
+                {/* <div className="max-w-7xl mx-auto">
+                    <About />
+                    <Projects />
+                    <Clients />
+                </div>
+                <Contact /> */}
+
+            </main>
+        </>
+    );
 }
